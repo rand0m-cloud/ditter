4d84ca797cf7e21d3828146ed9caa595441b8959
Abby Bryant <rand0m-cloud@outlook.com>
2023-07-29 16:11:32 -0400

    login page

--- ditterapp/src/App.vue
+++ ditterapp/src/App.vue
@@ -1,18 +1,39 @@
 <script setup>
 import { RouterView } from "vue-router";
-import { onMounted } from "vue";
+import { onMounted, computed } from "vue";
 import API from "./API.js";
+import router from "./router";
 
 onMounted(() => {
   API.checkSessionStorage();
 });
+
+const login_status = computed(() => {
+  if (API.user.value) {
+    return "logged-in";
+  } else {
+    return "logged-out";
+  }
+});
+
+const logout = () => API.logout();
+const login = () => router.push("/login");
 </script>
 
 <template>
   <v-app>
-      <v-app-bar class="bg-orange" title="Ditter"/>
-      <v-main>
-        <RouterView />
-      </v-main>
+    <v-app-bar class="bg-orange" title="Ditter">
+      <template v-slot:append>
+        <v-btn v-if="login_status == `logged-in`" @click="logout">
+          <v-icon>mdi-logout</v-icon>Logout
+        </v-btn>
+        <v-btn v-if="login_status == `logged-out`" @click="login">
+          <v-icon>mdi-login</v-icon>Login
+        </v-btn>
+      </template>
+    </v-app-bar>
+    <v-main>
+      <RouterView />
+    </v-main>
   </v-app>
 </template>
--- ditterapp/src/views/LoginView.vue
+++ ditterapp/src/views/LoginView.vue
@@ -20,4 +20,40 @@ const submit = (e) => {
 };
 </script>
 
-<template></template>
+<template>
+  <div class="bg-blue h-100">
+    <v-container fluid>
+      <v-row justify="space-around">
+        <v-col xs="12" sm="10" md="8" lg="6" xl="4">
+          <v-sheet rounded class="pa-2" elevation="8">
+            <v-form @submit.prevent>
+              <v-container fluid>
+                <v-row v-if="error">
+                  <v-col>
+                    <v-alert :text="error" class="bg-error" />
+                  </v-col>
+                </v-row>
+                <v-row justify="space-around">
+                  <v-col>
+                    <v-text-field v-model="username" label="Username" autocomplete="username" />
+                    <v-text-field
+                      v-model="password"
+                      label="Password"
+                      type="password"
+                      autocomplete="password"
+                    />
+                  </v-col>
+                </v-row>
+                <v-row justify="space-around">
+                  <v-col sm="6" md="6">
+                    <v-btn block type="submit" @click="submit">Login</v-btn>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-form>
+          </v-sheet>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
